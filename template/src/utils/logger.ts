2a0ec0ca779a81dffa4cad82218549a861d1a051
Ayush Dixit <fsayush100@gmail.com>
2026-05-02 21:22:24 +0530

    Update logger.ts

--- template/src/utils/logger.ts
+++ template/src/utils/logger.ts
@@ -1,20 +1,6 @@
 import pino from "pino";
 import { NODE_ENV } from "../config/env.js";
 
-// export const logger = pino({
-//   level: NODE_ENV === "production" ? "info" : "debug",
-//   transport: NODE_ENV === "development"
-//     ? {
-//         target: "pino-pretty",
-//         options: {
-//           colorize: true,
-//           translateTime: "SYS:standard",
-//           ignore: "pid,hostname",
-//         },
-//       }
-//     : undefined,
-// });
-
 export const logger = pino({
   level: NODE_ENV === "production" ? "info" : "debug",
   transport:
@@ -23,6 +9,7 @@ export const logger = pino({
           target: "pino-pretty",
           options: {
             colorize: true,
+            colorizeObjects: false,
             translateTime: "HH:MM:ss",
             ignore: "pid,hostname",
             singleLine: true,
